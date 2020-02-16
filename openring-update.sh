147005270cbc485769802a182eedc59c0c53d88d
Simon Ser <contact@emersion.fr>
2020-02-16 12:22:42 +0100

    Switch all openring feed URLs to HTTPS

--- openring-update.sh
+++ openring-update.sh
@@ -6,10 +6,10 @@ mkdir -p data
 go run openring.go >data/openring.toml \
 	-s "https://drewdevault.com/feed.xml" \
 	-s "https://ppaalanen.blogspot.com/feeds/posts/default?alt=rss" \
-	-s "http://blog.ffwll.ch/feed.xml" \
+	-s "https://blog.ffwll.ch/feed.xml" \
 	-s "https://www.fooishbar.org/index.xml" \
 	-s "https://idea.popcount.org/rss.xml" \
 	-s "https://mstoeckl.com/notes/gsoc/rss.xml" \
-	-s "http://research.swtch.com/feed.atom" \
+	-s "https://research.swtch.com/feed.atom" \
 	-s "https://ewontfix.com/feed.rss" \
 	-s "https://rosenzweig.io/blog/gpu-feed.xml"
