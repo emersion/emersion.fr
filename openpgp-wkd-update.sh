9b78e9d97a3ee27086d26b76ffb94e177c4d88ae
Simon Ser <contact@emersion.fr>
2021-03-10 19:04:13 +0100

    openpgp-wkd-update: hardcode WKD hash
    This removes the dependency on openpgp-wkd-local.

--- openpgp-wkd-update.sh
+++ openpgp-wkd-update.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/sh -eu
 
-cd static/.well-known/openpgpkey
-gpg --export 34FF9526CFEF0E97A340E2E40FDE7BE0E88F5E48 | openpgp-wkd-local add
+# Get these with:
+# gpg --list-keys --with-wkd-hash
+key_id=34FF9526CFEF0E97A340E2E40FDE7BE0E88F5E48
+wkd_hash=dj3498u4hyyarh35rkjfnghbjxug6b19
+
+gpg --export "$key_id" >static/.well-known/openpgpkey/hu/"$wkd_hash"
